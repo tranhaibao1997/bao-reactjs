f0ed1d228da62a967c4e3b36bbe0041a8b645b32
Tran Hai Bao <tranhaibao1997@gmail.com>
2019-11-30 12:44:30 +0700

    add item

--- src/App.js
+++ src/App.js
@@ -5,10 +5,41 @@ function App() {
     return ( <
         div className = "App" >
         <
-        p > Hello ReactJs < /p> <
-        p > Hai Bao < /p> <
+        Item > < /Item> <
         /div>
+
     );
 }
+
+var Item = () => ( <
+    div className = "item-wrapper" >
+    <
+    div className = "image-container" >
+    <
+    img className = ""
+    src = "http://www.tacer.biz/sites/default/files/018-Wing_Back_Chair%5B1%5D.jpg"
+    alt = "" / >
+    <
+    /div> <
+    div className = "sale-tag" >
+    <
+    p > Sale < /p> <
+    /div> <
+    div class = "item-info" >
+
+    <
+    p class = "item-category" > FURNITURE < /p> <
+    p class = "item-name" > Minimal Deco Furniture < /p> <
+    div class = "item-price" >
+    <
+    p > $119 USD < /p> <
+    p id = "break-through" > $230 USD < /p> <
+    /div> <
+    /div>
+
+    <
+    /div>
+);
+
 
 export default App;
