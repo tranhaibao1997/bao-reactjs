b20c3e4e6d2ecd7e05815d1587b8299785bd6270
Tran Hai Bao <tranhaibao1997@gmail.com>
2019-11-30 15:52:28 +0700

    chia component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,39 @@
 import React from 'react';
 import './App.css';
 
+import { ProductImage, ProductInfo } from "./Item";
+
 function App() {
     return ( <
         div className = "App" >
         <
-        Item > < /Item> <
-        Item > < /Item> <
-        Item > < /Item> <
+        Item src = "http://www.tacer.biz/sites/default/files/018-Wing_Back_Chair%5B1%5D.jpg"
+        saleText = "20"
+        price = "230.00 USD"
+        finalPrice = "119.00 USD"
+        name = "Minimal Deco Furniture"
+        type = "FURNITURE" >
+        < /Item>
+
+        <
         /div>
 
     );
 }
 
-var Item = () => ( <
-    div className = "item-wrapper" >
-    <
-    div className = "image-container" >
-    <
-    img className = ""
-    src = "http://www.tacer.biz/sites/default/files/018-Wing_Back_Chair%5B1%5D.jpg"
-    alt = "" / >
-    <
-    /div> <
-    div className = "sale-tag" >
-    <
-    p > Sale < /p> <
-    /div> <
-    div class = "item-info" >
-
-    <
-    p class = "item-category" > FURNITURE < /p> <
-    p class = "item-name" > Minimal Deco Furniture < /p> <
-    div class = "item-price" >
-    <
-    p > $119 USD < /p> <
-    p id = "break-through" > $230 USD < /p> <
-    /div> <
-    /div>
-
-    <
-    /div>
-);
+function Item(props) {
+    return ( <
+        div class = "item-wrapper" >
+        <
+        ProductImage src = { props.src }
+        saleText = { props.saleText } > < /ProductImage> <
+        ProductInfo price = { props.price }
+        finalPrice = { props.finalPrice }
+        name = { props.name }
+        type = { props.type } > < /ProductInfo> <
+        /div>
+    );
+}
 
 
 export default App;
---
+++ src/Item.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export function ProductImage(props)
+{ return(
+<div class="product-Image">
+<div className="image-container">
+        <img src={props.src} alt="" />
+      </div>
+      <div className="sale-tag">
+        <p>{props.saleText}</p>
+      </div>
+      </div>
+)
+}
+
+
+export  function ProductInfo(props)
+{ return(
+<div class="item-info">
+
+<p class="item-category">{props.type}</p>
+<p class="item-name">{props.name}</p>
+<div class="item-price">
+  <p>{props.finalPrice}</p>
+  <p id="break-through">{props.price}</p>
+</div>
+</div>
+)
+}
+
