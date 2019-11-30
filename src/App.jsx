fcf6f6d8e72c09a49413a1f6f0e2bd2c1acd2e2b
Tran Hai Bao <tranhaibao1997@gmail.com>
2019-11-30 16:01:31 +0700

    fix component

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+import { ProductImage, ProductInfo } from "./Item";
+
+function App() {
+  return (
+    <div className="App">
+      <Item src="http://www.tacer.biz/sites/default/files/018-Wing_Back_Chair%5B1%5D.jpg"
+        saleText="20" price="230.00 USD" finalPrice="119.00 USD" name="Minimal Deco Furniture" type="FURNITURE"
+      ></Item>
+
+    </div>
+
+  );
+}
+
+function Item(props) {
+  return (
+    <div class="item-wrapper">
+      <ProductImage src={props.src} saleText={props.saleText}></ProductImage>
+      <ProductInfo price={props.price} finalPrice={props.finalPrice} name={props.name} type={props.type}></ProductInfo>
+    </div>
+  );
+}
+
+
+export default App;
