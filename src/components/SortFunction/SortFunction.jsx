a1e48390fcc9e51522cdc57431795a87f24b3ce8
Tran Hai Bao <tranhaibao1997@gmail.com>
2020-01-16 11:32:09 +0700

    fix sort

--- src/components/SortFunction/SortFunction.jsx
+++ src/components/SortFunction/SortFunction.jsx
@@ -1,5 +1,6 @@
 import React,{useContext,useEffect} from 'react'
 import { ThemeContext } from '../../ThemeContext';
+import Axios from 'axios';
 
 function SortFunction(props) 
 {
@@ -9,33 +10,113 @@ function SortFunction(props)
   }, [])
   console.log(props.data,"from sort")
 
-  var newProducts = [...props.data]
 
   function AtoZ() {
-
-    props.getProductListBySort(newProducts.sort((a, b) => a.name.localeCompare(b.name)));
+    return async()=>
+    {
+     try {
+       let key_word = props.metadata.key_word;
+       let page_num = props.metadata.current_page;
+       const result = await Axios({
+         method: "GET",
+         url: `https://mapi.sendo.vn/mob/product/search?p=${page_num}&q=${key_word}`
+       })
+       console.log(result)
+       let newArray = [...result.data.data]
+       props.getProductListBySort(newArray.sort((a, b) => a.name.localeCompare(b.name)));
+ 
+     } catch (error) {
+       console.log(error)
+     }
+    }
+    
   
   }
   function ZtoA() {
-    
-    props.getProductListBySort(newProducts.sort((a, b) => b.name.localeCompare(a.name)));
+    return async()=>
+    {
+     try {
+       let key_word = props.metadata.key_word;
+       let page_num = props.metadata.current_page;
+       const result = await Axios({
+         method: "GET",
+         url: `https://mapi.sendo.vn/mob/product/search?p=${page_num}&q=${key_word}`
+       })
+       console.log(result)
+       let newArray = [...result.data.data]
+       props.getProductListBySort(newArray.sort((a, b) => b.name.localeCompare(a.name)));
+ 
+     } catch (error) {
+       console.log(error)
+     }
+    }
+  
    
   }
   function LowToHigh() {
+    return async()=>
+    {
+     try {
+       let key_word = props.metadata.key_word;
+       let page_num = props.metadata.current_page;
+       const result = await Axios({
+         method: "GET",
+         url: `https://mapi.sendo.vn/mob/product/search?p=${page_num}&q=${key_word}`
+       })
+       console.log(result)
+       let newArray = [...result.data.data]
+       props.getProductListBySort(newArray.sort((a, b) => a.final_price-b.final_price));
+ 
+     } catch (error) {
+       console.log(error)
+     }
+    }
     
-    props.getProductListBySort(newProducts.sort((a, b) => a.final_price-b.final_price));
    
   }
   function HighToLow() {
-
-    props.getProductListBySort(newProducts.sort((a, b) => b.final_price-a.final_price));
+    return async()=>
+    {
+     try {
+       let key_word = props.metadata.key_word;
+       let page_num = props.metadata.current_page;
+       const result = await Axios({
+         method: "GET",
+         url: `https://mapi.sendo.vn/mob/product/search?p=${page_num}&q=${key_word}`
+       })
+       console.log(result)
+       let newArray = [...result.data.data]
+       props.getProductListBySort(newArray.sort((a, b) => b.final_price-a.final_price));
+ 
+     } catch (error) {
+       console.log(error)
+     }
+    }
+    
    
   }
   function TopSales() {
-
- props.getProductListBySort(newProducts.filter(a => a.promotion_percent >35));
- console.log(newProducts.filter(a => a.promotion_percent >50))
+    return async()=>
+    {
+     try {
+       let key_word = props.metadata.key_word;
+       let page_num = props.metadata.current_page;
+       const result = await Axios({
+         method: "GET",
+         url: `https://mapi.sendo.vn/mob/product/search?p=${page_num}&q=${key_word}`
+       })
+       console.log(result)
+       let newArray = [...result.data.data]
+       let filteredArray = newArray.filter(a => a.promotion_percent > 35)
+       console.log(filteredArray)
+       await props.getProductListBySort(filteredArray)
+ 
+     } catch (error) {
+       console.log(error)
+     }
+    }
    }
+ 
   
 
  
@@ -47,11 +128,11 @@ function SortFunction(props)
     <div className="shop-widget">
       <h3 className="shop-title">SHOP BY</h3>
       <ul className="shop-link">
-        <li onClick={AtoZ}><a href="#">Name: A-Z</a></li>
-        <li onClick={ZtoA}><a href="#">Name: Z-A</a></li>
-        <li onClick={HighToLow}><a href="#">Price: High to Low</a></li>
-        <li onClick={LowToHigh}><a href="#">Price: Low to High</a></li>
-        <li onClick={TopSales}><a href="#">Product: Top Sales</a></li>
+        <li onClick={AtoZ()}><a href="#">Name: A-Z</a></li>
+        <li onClick={ZtoA()}><a href="#">Name: Z-A</a></li>
+        <li onClick={HighToLow()}><a href="#">Price: High to Low</a></li>
+        <li onClick={LowToHigh()}><a href="#">Price: Low to High</a></li>
+        <li onClick={TopSales()}><a href="#">Product: Top Sales</a></li>
       </ul>
     </div>
 
